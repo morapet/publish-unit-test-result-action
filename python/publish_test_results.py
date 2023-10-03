d286022a32c097be7ac7ac99c79c1d4382998d60
Peter Moran <peter.moran@deutsche-boerse.com>
2023-10-03 11:35:46 +0000

    fix missing arg

--- python/publish_test_results.py
+++ python/publish_test_results.py
@@ -508,7 +508,8 @@ def get_settings(options: dict, gha: Optional[GithubAction] = None) -> Settings:
         seconds_between_github_reads=float(seconds_between_github_reads),
         seconds_between_github_writes=float(seconds_between_github_writes),
         search_pull_requests=get_bool_var('SEARCH_PULL_REQUESTS', options, default=False),
-        details_url = get_var('DETAILS_URL', options)
+        details_url = get_var('DETAILS_URL', options),
+        conclusion = get_var('CONCLUSION', options)
     )
 
     check_var(settings.token, 'GITHUB_TOKEN', 'GitHub token')
